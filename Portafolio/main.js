12ca0d06427af3fd84c7085dab0195868b6eef57
1w1111957TodescoNatalia <69582069+1w1111957TodescoNatalia@users.noreply.github.com>
2021-07-27 21:57:28 -0300

    Portafolio
    primer commir

---
+++ Portafolio/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+    progreso();
+})
+function progreso() {
+    let scroll = document.documentElement.scrollTop;
+    let alto = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let progreso = (scroll/alto)*100;
+    document.getElementsByClassName("barra")[0].style.width = progreso+"%";
+}
